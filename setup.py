ad7f2915f75b51ed3469d814e719982209b010b0
Charlies VIDS <chuckdustin12@gmail.com>
2023-10-31 18:57:18 -0700

    added __init__

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
+# Read requirements.txt and remove any comments
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+    requirements = [r.strip() for r in requirements if not r.startswith('#')]
+
 setup(
     name='fudstop',
-    version='0.1.2',
+    version='0.1.5',
     packages=find_packages(),
-    install_requires=[
-        # list your project dependencies here
-    ],
+    install_requires=requirements,
 )
